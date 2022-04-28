88f26c75d2a221fa60a79e3beb626f6736bc718e
rui <azernia@foxmail.com>
2022-04-28 12:04:13 +0800

    测试

--- data-struct/stack/linked-stack/src/main.c
+++ data-struct/stack/linked-stack/src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../head/LinkedStack.h"
+#include "../head/LinkedStackData.h"
+#include "LinkedStack.c"
 
 int main()
 {
+    LinkedStatck *stack = (LinkedStatck *)malloc(sizeof(LinkedStatck));
+    initLinkedStack(stack);
+    push(stack, 1);
+    ElementType element;
+    pop(stack, &element);
+    printf("pop result = %d\n", element);
+    free(stack);
     return 0;
 }
