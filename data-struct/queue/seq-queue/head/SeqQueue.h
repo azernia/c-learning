aa3e1920d09152d145e26f70be4678fa6f168367
rui <azernia@foxmail.com>
2022-04-28 14:12:57 +0800

    入队

--- data-struct/queue/seq-queue/head/SeqQueue.h
+++ data-struct/queue/seq-queue/head/SeqQueue.h
@@ -9,4 +9,12 @@
  */
 void init(SeqQueue *queue);
 
+/**
+ * 入队
+ * @param queue 操作的队列
+ * @param element 入队的元素
+ * @return int 0-失败 1-成功
+ */
+int enqueue(SeqQueue *queue, ElementType element);
+
 #endif
--- data-struct/queue/seq-queue/src/SeqQueue.c
+++ data-struct/queue/seq-queue/src/SeqQueue.c
@@ -8,3 +8,10 @@ void init(SeqQueue *queue)
     queue->top = 0;
     queue->rear = queue->top;
 }
+
+int enqueue(SeqQueue *queue, ElementType element)
+{
+    queue->data[queue->rear] = element;
+    queue->rear++;
+    return TRUE;
+}
