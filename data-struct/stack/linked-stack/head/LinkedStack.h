2eef328a45f35846528eb1d8eee6356a7d1db758
rui <azernia@foxmail.com>
2022-04-28 10:22:56 +0800

    压栈

--- data-struct/stack/linked-stack/head/LinkedStack.h
+++ data-struct/stack/linked-stack/head/LinkedStack.h
@@ -9,4 +9,12 @@
  */
 void initLinkedStack(LinkedStatck *stack);
 
+/**
+ * 压栈（入栈）
+ * @param stack 操作的栈
+ * @param element 入栈元素
+ * @return int 0-成功 1-失败
+ */
+int push(LinkedStatck *stack, ElementType element);
+
 #endif
--- data-struct/stack/linked-stack/src/LinkedStack.c
+++ data-struct/stack/linked-stack/src/LinkedStack.c
@@ -8,3 +8,15 @@ void initLinkedStack(LinkedStatck *stack)
     stack->top = NULL;
     stack->length = 0;
 }
+
+int push(LinkedStatck *stack, ElementType element)
+{
+    // 创建新结点
+    LinkedStatckNode *node = (LinkedStatckNode *)malloc(sizeof(LinkedStatckNode));
+    node->data = element;
+    // 指向上一个元素
+    node->next = stack->top;
+    stack->top = node;
+    stack->length++;
+    return TRUE;
+}
