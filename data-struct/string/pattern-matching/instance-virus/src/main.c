b469466514d7b3b96312cb45e098d3df0a9222b9
rui <azernia@foxmail.com>
2022-06-07 13:32:38 +0800

    KMP实例

--- data-struct/string/pattern-matching/instance-virus/src/main.c
+++ data-struct/string/pattern-matching/instance-virus/src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../head/check.h"
+#include "check.c"
 
 int main()
 {
+    char *pattern = "ababaa";
+    int next[strlen(pattern)];
+    getNext(pattern, next);
+    for (int i = 0; i < strlen(pattern); ++i)
+    {
+        printf("%d ", next[i]);
+    }
     return 0;
 }
